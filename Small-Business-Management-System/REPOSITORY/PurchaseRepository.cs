d1b7bb1fa7f55728cd9da23a87b80380b7d75f78
Shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-10-13 10:21:24 +0600

    PurchaseForm

---
+++ Small-Business-Management-System/REPOSITORY/PurchaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Small_Business_Management_System.REPOSITORY
+{
+    class PurchaseRepository
+    {
+    }
+}
